a35e8e8eec25b8854c352ca523277a6550cd8b1e
thagberg <timothy.m.hagberg@gmail.com>
2016-01-30 19:03:04 -0800

    Add some clutter items

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusic : MonoBehaviour {
+	
+	private static BackgroundMusic instance = null;
+	public static BackgroundMusic Instance {
+		get { return instance; }
+	}
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		// The background music is assumed to be attached as an AudioSource.
+		AudioSource bgMusic = GetComponent<AudioSource> ();
+		bgMusic.Play ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
