41e0d2b1a933b5f1b36b67955f38e02fb21b47e2
thagberg <timothy.m.hagberg@gmail.com>
2016-01-30 18:04:23 -0800

    More dialogue

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -15,10 +15,14 @@ public class Dialogue : MonoBehaviour{
 		lines["Lazy Father"].Add("ScholarlyBook", "*Hack hack* What am I supposed to do with this garbage?!");
 
 		lines.Add("Lazy Mother", new Dictionary<string, string>());
-		lines["Lazy Mother"].Add("", "I have nothing to say.");
+		lines["Lazy Mother"].Add("", "Joffrey, go fetch me some food.  I absolutely MUST eat this moment!");
+		lines["Lazy Mother"].Add("RawChicken", "*nom nom nom*  Oooh!  I feel so fancy!\n\n*gurgle*  Ugh... I don't feel so good...");
+		lines["Lazy Mother"].Add("CookedChicken", "Well, I guess this will _have_ to do...");
 
 		lines.Add("Lazy Child", new Dictionary<string, string>());
-		lines["Lazy Child"].Add("", "I have nothing to say.");
+		lines["Lazy Child"].Add("", "I'm booooooored Joffrey... Find me something to play with, NOW!");
+		lines["Lazy Child"].Add("ActionFigure", "I'm BORED of playing with this toy!");
+		lines["Lazy Child"].Add("Scissors", "Yay!  Mummy would never let me play with these!  Whee!\n\nOops! *SLICE*");
 	}
 
 	public string getDialogue(string name, Robot r) {
