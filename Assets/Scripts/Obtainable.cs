6a3d6d016d532374658d07570145cdd88c918151
thagberg <timothy.m.hagberg@gmail.com>
2016-01-30 20:03:29 -0800

    Bleh

--- Assets/Scripts/Obtainable.cs
+++ Assets/Scripts/Obtainable.cs
@@ -4,6 +4,8 @@ using System.Collections;
 
 public class Obtainable : Clickable {
 
+	public bool used = false;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -15,16 +17,18 @@ public class Obtainable : Clickable {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		GameObject robot = other.gameObject;
-		if (robot.name.Equals("Robot Butler")) {
-			Robot r = robot.GetComponent<Robot>();
-			GameObject dialogueText = GameObject.Find("Dialogue Text");
-			Text t = dialogueText.GetComponent<Text>();
-			if (r.holdingObjectName == null || r.holdingObjectName.Equals("")) {
-				r.holdingObjectName = gameObject.name;
-				t.text = "Oh... great... I've got a " + gameObject.name;
-			} else {
-				t.text = "*Sigh*  I can only hold _so_ many items, I'm afraid...";
+		if (!used) {
+			GameObject robot = other.gameObject;
+			if (robot.name.Equals("Robot Butler")) {
+				Robot r = robot.GetComponent<Robot>();
+				GameObject dialogueText = GameObject.Find("Dialogue Text");
+				Text t = dialogueText.GetComponent<Text>();
+				if (r.holdingObjectName == null || r.holdingObjectName.Equals("")) {
+					r.holdingObjectName = gameObject.name;
+					t.text = "Oh... great... I've got a " + gameObject.name;
+				} else {
+					t.text = "*Sigh*  I can only hold _so_ many items, I'm afraid...";
+				}
 			}
 		}
 	}
